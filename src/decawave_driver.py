cda1a3da38ba945907b26cfef0ca28c3a590338f
ktg10 <ktg10@case.edu>
2015-10-21 17:12:59 -0400

    Update 1

--- src/decawave_driver.py
+++ src/decawave_driver.py
@@ -22,3 +22,54 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
+import rospy
+from snowmower_msgs.msg import DecaWaveMsg
+
+import serial, time, os
+from serial import SerialException
+
+class DecaWaveDriver:
+  
+  def __init__(self):
+    rospy.init_node('decawave_driver')
+    dwPub = rospy.Publisher('dw/data', DecaWaveMsg, queue_size = 5)
+    try:
+      ser = serial.Serial(
+      port = 5,
+      timeout = 1,
+      baudrate = 115200
+      )
+      
+      dwMsg = DecaWaveMsg()
+      dwMsg.header.frame_id = "base_decawave"
+      
+      ser.close()
+      ser.open()
+      
+      d0 = 0
+      d1 = 0
+      d2 = 0
+      d3 = 0
+      while not rospy.is_shutdown():
+        i = 0
+        while i < 3:
+          raw_data = ser.read(size = 56)
+          if raw_data[1:4] == b'a00':
+            d0 = int(raw_data[9:17], 16) / 1000.00
+          elif raw_data[1:4] == b'a01':
+            d1 = int(raw_data[9:17], 16) / 1000.00
+          elif raw_data[1:4] == b'a02':
+            d2 = int(raw_data[9:17], 16) / 1000.00
+          elif raw_data[1:4] == b'a03':
+            d3 = int(raw_data[9:17], 16) / 1000.00
+          i+=1
+          
+        dwMsg.dist = (d0, d1, d2, d3)
+        dwMsg.header.stamp = rospy.get_rostime()
+        dwPub.publish(dwMsg)
+      
+      ser.close()
+      
+    except SerialException:
+      print("Could not connect to the serial port")
+    
